3340cd3cd6e952cb0de97adc8de03535827abfe3
Ujjwol <32824171+ujjwol04@users.noreply.github.com>
2017-12-24 10:56:51 -0600

    Update best.R

--- best.R
+++ best.R
@@ -1,5 +1,3 @@
-setwd("C:/UJJWOL/Downloads/Other R files/R- Coursera/rprog%2Fdata%2FProgAssignment3-data")
-
 data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
 
 best <- function(state,outcome) {
